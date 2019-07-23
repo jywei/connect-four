d58f1b392f436a895f43bb4c948ab0330dc91afe
Roy Wei <roywjy@gmail.com>
2019-07-23 22:11:08 +1000

    Require setup and game

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -3,6 +3,8 @@ require_relative "connect_four/grid"
 require_relative "connect_four/player"
 require_relative "connect_four/human"
 require_relative "connect_four/computer"
+require_relative "connect_four/setup"
+require_relative "connect_four/game"
 
 board = Board.new
 grid = Grid.new(board)
--- run.rb
+++ run.rb
@@ -2,3 +2,5 @@ require_relative "lib/connect_four"
 require "pry"
 
 puts "game started"
+
+Setup.new.execute
