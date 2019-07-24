b174626ac2bdc8f8d3086c0425aee6f8061e3ebf
Roy Wei <roywjy@gmail.com>
2019-07-24 22:04:37 +1000

    Add check_diagonals and restart game

--- lib/connect_four/game.rb
+++ lib/connect_four/game.rb
@@ -30,10 +30,17 @@ class Game
     end
   end
 
+  def restart_game
+    board.create_board
+    start_game
+  end
+
   def type_yes_or_no
     grid.print_board
     puts "Please enter 'y' or 'n'"
   end
+
+  private
 
   def main_game_function
     loop do
@@ -63,6 +70,7 @@ class Game
 
   def check_winner(player)
     check_rows_and_columns(player)
+    check_diagonals(player)
   end
 
   def check_rows_and_columns(player)
@@ -70,10 +78,41 @@ class Game
     check_arrays(board.two_dimensional_arrays.transpose, player)
   end
 
+  def check_diagonals(player)
+    diagonal_winning_options.each do |direction, starting_point|
+      starting_point.each do |start|
+        array_for_testing = []
+        (0..3).each do |move|
+          array_for_testing << board.two_dimensional_arrays[start[0] + move][start[1] + (move * direction)]
+        end
+        checking_four_in_a_row(array_for_testing, player)
+      end
+    end
+  end
+
   def check_arrays(arrays, player)
     arrays.each do |row|
-      winner(player) if row.join =~ /#{player.symbol}{4}/
+      checking_four_in_a_row(row, player)
     end
+  end
+
+  def checking_four_in_a_row(array, player)
+    winner(player) if array.join =~ /#{player.symbol}{4}/
+  end
+
+  def diagonal_winning_options
+    {
+      1 => [
+        [0, 0], [0, 1], [0, 2], [0, 3],
+        [1, 0], [1, 1], [1, 2], [1, 3],
+        [2, 0], [2, 1], [2, 2], [2, 3]
+      ],
+      -1 => [
+        [0, 3], [0, 4], [0, 5], [0, 6],
+        [1, 3], [1, 4], [1, 5], [1, 6],
+        [2, 3], [2, 4], [2, 5], [2, 6]
+      ]
+    }
   end
 
 end
