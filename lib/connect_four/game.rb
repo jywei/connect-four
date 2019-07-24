aaa10f592543f2ee2933eff2ac3e9467465ceaee
Roy Wei <roywjy@gmail.com>
2019-07-24 23:41:33 +1000

    Move up diagonal_winning_options

--- lib/connect_four/game.rb
+++ lib/connect_four/game.rb
@@ -2,6 +2,19 @@ class Game
 
   attr_accessor :board, :grid, :players
 
+  DIAGONAL_WINNING_OPTIONS = {
+    1 => [
+      [0, 0], [0, 1], [0, 2], [0, 3],
+      [1, 0], [1, 1], [1, 2], [1, 3],
+      [2, 0], [2, 1], [2, 2], [2, 3]
+    ],
+    -1 => [
+      [0, 3], [0, 4], [0, 5], [0, 6],
+      [1, 3], [1, 4], [1, 5], [1, 6],
+      [2, 3], [2, 4], [2, 5], [2, 6]
+    ]
+  }.freeze
+
   def initialize(board, grid, players)
     @board   = board
     @grid    = grid
@@ -109,18 +122,7 @@ class Game
   end
 
   def diagonal_winning_options
-    {
-      1 => [
-        [0, 0], [0, 1], [0, 2], [0, 3],
-        [1, 0], [1, 1], [1, 2], [1, 3],
-        [2, 0], [2, 1], [2, 2], [2, 3]
-      ],
-      -1 => [
-        [0, 3], [0, 4], [0, 5], [0, 6],
-        [1, 3], [1, 4], [1, 5], [1, 6],
-        [2, 3], [2, 4], [2, 5], [2, 6]
-      ]
-    }
+    DIAGONAL_WINNING_OPTIONS
   end
 
   def print_board_and_display_move
