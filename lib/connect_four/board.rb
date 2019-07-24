0ed34b8cdaa8e558deeef9fcac34a412074006a7
Roy Wei <roywjy@gmail.com>
2019-07-24 16:32:33 +1000

    Change empty array from - to .

--- lib/connect_four/board.rb
+++ lib/connect_four/board.rb
@@ -7,20 +7,20 @@ class Board
   end
 
   def create_board
-    @two_dimensional_arrays = Array.new(6) { Array.new(7) { "-" } }
+    @two_dimensional_arrays = Array.new(6) { Array.new(7) { "." } }
   end
 
   def fill_column(column, player, grid)
     # fill symbols from buttom to the top
     @two_dimensional_arrays.reverse.each do |row|
-      next unless row[column] == "-"
+      next unless row[column] == "."
       row[column] = player.symbol
       break
     end
   end
 
   def can_fill?(column)
-    @two_dimensional_arrays.map { |row| row[column] }.any? { |slot| slot == "-" }
+    @two_dimensional_arrays.map { |row| row[column] }.any? { |slot| slot == "." }
   end
 
 end
